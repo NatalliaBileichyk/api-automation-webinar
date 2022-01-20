f1b81ac6a6e48787caf1c84116a4cfa31b3e2b5f
Bileichyk, Natallia <Natallia.Bileichyk@wolterskluwer.com>
2022-01-21 00:01:07 +0300

    Add checks for albums and comments; updating posts

---
+++ specs/comments.spec.js
@@ -0,0 +1,255 @@
+'use strict';
+
+const chakram = require('chakram');
+const expect = chakram.expect;
+const api = require('./utils/api');
+const data = require('../server/data.json');
+
+describe.only('Comments', () => {
+       
+    describe('CREATE', () => {
+
+        afterEach(() => {
+            if (newCommentsId) {                
+                chakram.delete(api.url('comments/' + newCommentsId));
+            }
+        });
+
+        let newCommentsId = 0;
+
+        it('should create a new comment with a new id', () => {
+            const numberOfComments = data.comments[data.comments.length-1].id
+            const response = chakram.post(api.url('comments'));
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.id).to.be.equal(numberOfComments + 1);
+                newCommentsId = comments.id
+            });
+            return chakram.wait();
+        });        
+
+        it('should create a new comment with \"postId\" property', () => {
+            const numberOfComments = data.comments.length;
+            const response = chakram.post(api.url('comments'),{
+                postId: 255
+            });
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.postId).to.be.equal(255);
+                expect(comments.id).to.be.greaterThan(numberOfComments) 
+                newCommentsId = comments.id
+            });
+        
+            return chakram.wait();
+        });
+
+        it('should create a new comment with \"name\" property', () => {
+            const numberOfComments = data.comments.length;
+            const newName = "Do you like to write comments, feedback or complains"
+            const response = chakram.post(api.url('comments'),{
+                name: newName,
+            });
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.name).to.be.equal(newName);
+                expect(comments.id).to.be.greaterThan(numberOfComments) 
+                newCommentsId = comments.id
+            });
+        
+            return chakram.wait();
+        });
+
+        it('should create a new comment with \"body\" property', () => {
+            const numberOfComments = data.comments.length;
+            const response = chakram.post(api.url('comments'),{
+                body: "HAPPY NEW YEAR",
+            });
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.body).to.be.equal("HAPPY NEW YEAR");
+                expect(comments.id).to.be.greaterThan(numberOfComments) 
+                newCommentsId = comments.id
+            });
+        
+            return chakram.wait();
+        });
+
+        it('should create a new comment with \"email\" property', () => {
+            const numberOfComments = data.comments.length;
+            const newEmail = "Natallia_Bileichyk@epam.com";
+            const response = chakram.post(api.url('comments'),{
+                email: newEmail,
+            });
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.email).to.be.equal(newEmail);
+                expect(comments.id).to.be.greaterThan(numberOfComments) 
+                newCommentsId = comments.id
+            });
+        
+            return chakram.wait();
+        });
+
+        it('should create a new comment without all properties', () => {
+            const numberOfComments = data.comments.length;
+            const response = chakram.post(api.url('comments'));        
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.id).to.be.greaterThan(numberOfComments)
+                newCommentsId = comments.id                
+            });            
+            return chakram.wait();
+        });
+
+        it('should not create a new comment with existed id', () => {
+            const response = chakram.post(api.url('comments'),{
+                id: data.comments[data.comments.length-1].id
+            });
+            return expect(response).to.have.status(500);
+        });
+        
+    }); 
+
+    describe('READ', () => {
+        it('should return data of all comments', () => {
+            const numberOfComments = data.comments.length
+            const response = chakram.get(api.url('comments'));
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.length).to.be.equal(numberOfComments);
+            });
+            return chakram.wait();
+        });
+
+        it('should return data of required comment', () => {
+            const requiredCommentID = 15;
+            const response = chakram.get(api.url('comments/' + requiredCommentID));            
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.id).to.be.equal(requiredCommentID);                
+            });
+            return chakram.wait();
+        });
+
+        it('should return data of comments excluding a particular ID', () => {
+            const exceptID = 1
+            const response = chakram.get(api.url('comments?id_ne=' + exceptID));
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', comments => {                
+                expect(comments[0].id).not.to.be.equal(exceptID);
+            });
+            return chakram.wait();
+        });
+        
+    });
+
+    describe('UPDATE', () => {        
+        it('should update \"postId\" property of the comment', () => {
+            const updatedCommentID = 15;
+            const newPostID = 255000000;
+            const response = chakram.put(api.url('comments/' + updatedCommentID),{
+                postId: newPostID
+            });
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.id).to.be.equal(updatedCommentID);
+                expect(comments.postId).to.be.equal(newPostID);
+            });        
+            return chakram.wait();
+        });
+
+        it('should update \"name\" property of the comment', () => {
+            const updatedCommentID = 35;
+            const response = chakram.put(api.url('comments/' + updatedCommentID),{
+                name: "I'm updating wait I want"
+            });
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.id).to.be.equal(updatedCommentID);
+                expect(comments.name).to.be.equal("I'm updating wait I want");
+            });        
+            return chakram.wait();
+        });
+
+        it('should update \"body\" property of the comment', () => {
+            const updatedCommentID = 67;
+            const response = chakram.put(api.url('comments/' + updatedCommentID),{
+                body: "Hello all. A day without smile is ....."
+            });
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.id).to.be.equal(updatedCommentID);
+                expect(comments.body).to.be.equal("Hello all. A day without smile is .....");
+            });        
+            return chakram.wait();
+        });
+
+        it('should not update \"id\" property of the comment', () => {
+            const updatedCommentID = data.comments[data.comments.length-1].id;
+            const newCommentID = updatedCommentID + 10;                
+
+            const response = chakram.put(api.url('comments/' + updatedCommentID),{
+                id: newCommentID,
+                name: "I'm trying to update comment ..."
+            });
+
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.id).not.to.be.equal(newCommentID);
+                expect(comments.id).to.be.equal(updatedCommentID);
+                expect(comments.name).to.be.equal("I'm trying to update comment ...");
+            });
+
+            return chakram.wait();
+        });
+
+        it('updated comment should have only specified properties', () => {
+            const updatedCommentID = data.comments[0].id;
+            const response = chakram.put(api.url('comments/' + updatedCommentID),{
+                postId: 255
+            });
+            
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', comments => {
+                expect(comments.id).to.be.equal(updatedCommentID);
+                expect(comments.postId).to.be.equal(255);
+            });
+            expect(response).not.to.have.json('data', comments => {
+                expect(comments.name)
+                expect(comments.body)
+                expect(comments.email)
+            })
+
+            return chakram.wait();
+        });
+
+        it('should throw an error for using not existed comment id', () => {
+            const updatedCommentID = data.comments[data.comments.length-1].id + 1000;
+            const response = chakram.put(api.url('comments/' + updatedCommentID),{
+                name: "I'm trying to update comment once more ..."
+            });
+            return expect(response).to.have.status(404);
+        });
+    });
+    
+    describe('DELETE', () => {
+        it('should delete a comment by using existed ID', () => {
+            const deletedCommentID = 60;
+            const response = chakram.delete(api.url('comments/' + deletedCommentID));
+            expect(response).to.have.status(200);
+            return response.then(() => {
+                const deleteCommentById = chakram.get(api.url('comments/' + deletedCommentID));
+                expect(deleteCommentById).to.have.status(404);
+                return chakram.wait();
+            });
+        });
+
+        it('should throw error if the comment ID does not exist', () => {
+            const notExistedID = -5;
+            const response = chakram.delete(api.url('comments/' + notExistedID));
+            expect(response).to.have.status(404);
+            return chakram.wait();
+        });
+    });
+    
+});
--- specs/posts.spec.js
+++ specs/posts.spec.js
@@ -10,201 +10,238 @@ describe('Posts', () => {
     describe('CREATE', () => {
 
         afterEach(() => {
-            if (newPostsId) {
-                //console.log("AFTER newPostsId:" + newPostsId)
+            if (newPostsId) {                
                 chakram.delete(api.url('posts/' + newPostsId));
             }
         });
 
         let newPostsId = 0;
 
-            it('should create a new post with all properties', () => {
-                const response = chakram.post(api.url('posts'),{
-                    userId: 255,
-                    id: 300,
-                    title: "NEW POST HELLO",
-                    body: "NEW POST HAPPY NEW YEAR"
-                });
-                expect(response).to.have.status(201);
-                expect(response).to.have.json('data', posts => {
-                    expect(posts.userId).to.be.equal(255);
-                    expect(posts.id).to.be.equal(300);
-                    expect(posts.title).to.be.equal("NEW POST HELLO");
-                    expect(posts.body).to.be.equal("NEW POST HAPPY NEW YEAR");
-
-                    newPostsId = posts.id
-                    //console.log("newPostsId:" + newPostsId)
-                });
-            
-                return chakram.wait();
+        it('should create a new post with a new id', () => {
+            const numberOfPosts = data.posts[data.posts.length-1].id
+            const response = chakram.post(api.url('posts'));
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.id).to.be.equal(numberOfPosts + 1);
+                newPostsId = posts.id
             });
+            return chakram.wait();
+        });        
 
-            it('should create a new post without all properties', () => {
-                const numberOfposts = data.posts.length;
-                const response = chakram.post(api.url('posts'));
-               
-                expect(response).to.have.status(201);
-                expect(response).to.have.json('data', posts => {
-                    expect(posts.id).to.be.greaterThan(numberOfposts)
-
-                    newPostsId = posts.id
-                    //console.log("newPostsId:" + newPostsId)
-                });
-                
-                return chakram.wait();
+        it('should create a new post with \"userId\" property', () => {
+            const numberOfPosts = data.posts.length;
+            const response = chakram.post(api.url('posts'),{
+                userId: 255
             });
-
-            it('should not create a new post with existed id', () => {
-                const response = chakram.post(api.url('posts'),{
-                    id: data.posts[data.posts.length-1].id
-                });
-                return expect(response).to.have.status(500);
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.userId).to.be.equal(255);
+                expect(posts.id).to.be.greaterThan(numberOfPosts) 
+                newPostsId = posts.id
             });
-    
-            it('should create a new post with a new id', () => {
-                const numberOfposts = data.posts[data.posts.length-1].id
-                //console.log("numberOfposts: "+ numberOfposts)
-                
-                const response = chakram.post(api.url('posts'));
-                expect(response).to.have.status(201);
-                expect(response).to.have.json('data', posts => {
-                    //console.log("response"+ posts.id)
-                    expect(posts.id).to.be.equal(numberOfposts + 1);
-                    newPostsId = posts.id
-                    //console.log("newPostsId:" + newPostsId)
-                });
-                
-                return chakram.wait();
-            });  
-            
+        
+            return chakram.wait();
+        });
+
+        it('should create a new post with \"title\" property', () => {
+            const numberOfPosts = data.posts.length;
+            const response = chakram.post(api.url('posts'),{
+                title: "NEW POST HELLO",
+            });
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.title).to.be.equal("NEW POST HELLO");
+                expect(posts.id).to.be.greaterThan(numberOfPosts) 
+                newPostsId = posts.id
+            });
+        
+            return chakram.wait();
+        });
+
+        it('should create a new post with \"body\" property', () => {
+            const numberOfPosts = data.posts.length;
+            const response = chakram.post(api.url('posts'),{
+                body: "NEW POST HAPPY NEW YEAR",
+            });
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.body).to.be.equal("NEW POST HAPPY NEW YEAR");
+                expect(posts.id).to.be.greaterThan(numberOfPosts) 
+                newPostsId = posts.id
+            });
+        
+            return chakram.wait();
+        });
+
+        it('should create a new post without all properties', () => {
+            const numberOfPosts = data.posts.length;
+            const response = chakram.post(api.url('posts'));        
+            expect(response).to.have.status(201);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.id).to.be.greaterThan(numberOfPosts)
+                newPostsId = posts.id                
+            });            
+            return chakram.wait();
+        });
+
+        it('should not create a new post with existed id', () => {
+            const response = chakram.post(api.url('posts'),{
+                id: data.posts[data.posts.length-1].id
+            });
+            return expect(response).to.have.status(500);
+        });
+        
     }); 
 
     describe('READ', () => {
         it('should return data of posts', () => {
-            const numberOfposts = data.posts.length
+            const numberOfPosts = data.posts.length
             const response = chakram.get(api.url('posts'));
-            
             expect(response).to.have.status(200);
             expect(response).to.have.json('data', posts => {
-                expect(posts.length).to.be.equal(numberOfposts);
+                expect(posts.length).to.be.equal(numberOfPosts);
             });
-
-            return chakram.wait();            
+            return chakram.wait();
         });
 
         it('should return data of required post', () => {
             const requiredPostID = 15;
-            const response = chakram.get(api.url('posts/' + requiredPostID));
-            
+            const response = chakram.get(api.url('posts/' + requiredPostID));            
             expect(response).to.have.status(200);
             expect(response).to.have.json('data', posts => {
                 expect(posts.id).to.be.equal(requiredPostID);                
             });
+            return chakram.wait();
+        });
 
-            return chakram.wait();            
+        it('should return data of posts from _start to _end', () => {
+            const startNumber = data.posts.length - 20
+            const endNumber = data.posts.length
+            const response = chakram.get(api.url('posts?_start=' + startNumber +'&_end=' + endNumber));
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.length).to.be.equal(endNumber - startNumber);
+            });
+            return chakram.wait();
+        });
+
+        it('should return data of limited number of posts', () => {
+            const startNumber = data.posts[5]
+            const limit = data.posts.length
+            const response = chakram.get(api.url('posts?_start=' + startNumber +'&_limit' + limit));
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.length).to.be.equal(limit);
+            });
+            return chakram.wait();
         });
         
     });
 
-    describe('UPDATE', () => {
-        
-            it('should update post properties', () => {
-                const updatedPostID = 15;
-                const response = chakram.put(api.url('posts/' + updatedPostID),{
-                    userId: 255,                    
-                    title: "I'm updating post",
-                    body: "Hello all. A day without smile is ....."
-                });
-                expect(response).to.have.status(200);
-                expect(response).to.have.json('data', posts => {
-                    expect(posts.userId).to.be.equal(255);
-                    expect(posts.id).to.be.equal(updatedPostID);
-                    expect(posts.title).to.be.equal("I'm updating post");
-                    expect(posts.body).to.be.equal("Hello all. A day without smile is .....");
-                });
+    describe('UPDATE', () => {        
+        it('should update \"userId\" property of the post', () => {
+            const updatedPostID = 15;
+            const response = chakram.put(api.url('posts/' + updatedPostID),{
+                userId: 255
+            });
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.id).to.be.equal(updatedPostID);
+                expect(posts.userId).to.be.equal(255);
+            });        
+            return chakram.wait();
+        });
+
+        it('should update \"title\" property of the post', () => {
+            const updatedPostID = 35;
+            const response = chakram.put(api.url('posts/' + updatedPostID),{
+                title: "I'm updating post"
+            });
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.id).to.be.equal(updatedPostID);
+                expect(posts.title).to.be.equal("I'm updating post");
+            });        
+            return chakram.wait();
+        });
+
+        it('should update \"body\" property of the post', () => {
+            const updatedPostID = 67;
+            const response = chakram.put(api.url('posts/' + updatedPostID),{
+                body: "Hello all. A day without smile is ....."
+            });
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.id).to.be.equal(updatedPostID);
+                expect(posts.body).to.be.equal("Hello all. A day without smile is .....");
+            });        
+            return chakram.wait();
+        });
+
+        it('should not update \"id\" property of the post', () => {
+            const updatedPostID = data.posts[data.posts.length-1].id;
+            const newPostID = updatedPostID + 10;                
+
+            const response = chakram.put(api.url('posts/' + updatedPostID),{
+                id: newPostID,
+                title: "I'm trying to update post once more ..."
+            });
+
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.id).not.to.be.equal(newPostID);
+                expect(posts.id).to.be.equal(updatedPostID);
+                expect(posts.title).to.be.equal("I'm trying to update post once more ...");
+            });
+
+            return chakram.wait();
+        });
+
+        it('updated post should have only specified properties', () => {
+            const updatedPostID = data.posts[0].id;
+            const response = chakram.put(api.url('posts/' + updatedPostID),{
+                userId: 255
+            });
             
-                return chakram.wait();
+            expect(response).to.have.status(200);
+            expect(response).to.have.json('data', posts => {
+                expect(posts.id).to.be.equal(updatedPostID);
+                expect(posts.userId).to.be.equal(255);
             });
+            expect(response).not.to.have.json('data', posts => {
+                expect(posts.title)
+                expect(posts.body)
+            })
 
-            it('should not update post id', () => {
-                const updatedPostID = data.posts[data.posts.length-1].id;
-                const newPostID = updatedPostID + 10;                
+            return chakram.wait();
+        });
 
-                const response = chakram.put(api.url('posts/' + updatedPostID),{
-                    id: newPostID,
-                    title: "I'm trying to update post once more ..."
-                });
-
-                expect(response).to.have.status(200);
-                expect(response).to.have.json('data', posts => {
-                    expect(posts.id).not.to.be.equal(newPostID);
-                    expect(posts.id).to.be.equal(updatedPostID);
-                    expect(posts.title).to.be.equal("I'm trying to update post once more ...");
-                });
-
-                return chakram.wait();
+        it('should throw an error for using not existed post id', () => {
+            const updatedPostID = data.posts[data.posts.length-1].id + 1000;
+            const response = chakram.put(api.url('posts/' + updatedPostID),{
+                title: "I'm trying to update post once more ..."
             });
-
-            it('updated post should have only specified properties', () => {
-                const updatedPostID = data.posts[0].id;
-                const response = chakram.put(api.url('posts/' + updatedPostID),{
-                    userId: 255
-                });
-                
-                expect(response).to.have.status(200);
-                expect(response).to.have.json('data', posts => {
-                    expect(posts.id).to.be.equal(updatedPostID);
-                    expect(posts.userId).to.be.equal(255);
-                });
-                expect(response).not.to.have.json('data', posts => {
-                    expect(posts.title)
-                    expect(posts.body)
-                })
-
-                return chakram.wait();
-            });
-
-            it('should throw an error for using not existed post id', () => {
-                const updatedPostID = data.posts[data.posts.length-1].id + 1000;
-                const response = chakram.put(api.url('posts/' + updatedPostID),{
-                    title: "I'm trying to update post once more ..."
-                });
-                return expect(response).to.have.status(404);
-            });
-    });        
+            return expect(response).to.have.status(404);
+        });
+    });
     
     describe('DELETE', () => {
-            it('should delete a post by using existed ID', () => {
-                const response = chakram.delete(api.url('posts/10'));
-                expect(response).to.have.status(200);
-                return response.then(() => {
-                    const deletePostById = chakram.get(api.url('posts/10'));
-                    expect(deletePostById).to.have.status(404);
-                    return chakram.wait();
-                });
-            });
-
-            it('should throw error if the post ID does not exist', () => {
-                const response = chakram.delete(api.url('posts/0'));
-                expect(response).to.have.status(404);
+        it('should delete a post by using existed ID', () => {
+            const deletedPostID = 94;
+            const response = chakram.delete(api.url('posts/' + deletedPostID));
+            expect(response).to.have.status(200);
+            return response.then(() => {
+                const deletePostById = chakram.get(api.url('posts/' + deletedPostID));
+                expect(deletePostById).to.have.status(404);
                 return chakram.wait();
             });
+        });
+
+        it('should throw error if the post ID does not exist', () => {
+            const response = chakram.delete(api.url('posts/0'));
+            expect(response).to.have.status(404);
+            return chakram.wait();
+        });
     });
-       
     
-});
-
-describe('Comments', () => {
-});
-
-describe('Users', () => {
-});
-
-describe('Todos', () => {
-});
-
-describe('Photos', () => {
-});
-
-describe('Albums', () => {
 });
